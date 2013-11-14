fa17bba2c747431456849be5bef3bdfe577b309c
Adrian Leal <alejandroleal228@hotmail.com>
2013-11-14 01:05:39 +0000

    initial commit

---
+++ .bash_history
@@ -0,0 +1,17 @@
+gem uninstall rails
+gem uninstall railties
+gem install rails -v 3.2.13
+mkdir rails_projects
+cd rails_projects
+rails new first_app
+cd first_app/
+subl Gemfile
+subl Gemfile
+subl Gemfile
+subfil Gemfile
+subfile Gemfile
+gem 'jquery-rails'
+bundle update
+bundle install
+rails server
+rails server
---
+++ rails_projects/first_app/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+FirstApp::Application.config.secret_token = 'cfef4a9465b7d727142e82565ac33c063353441ec31c95ab8010c2e2316cdbd4104beeedccdd7487501bcd3e4d7917ab157025451a08cfeb8a606802d79fd14f'
